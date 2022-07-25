20136913d71ca772c505bc686b068fe46823dbe7
YexuanGuo <yexuan.guo@gmail.com>
2022-07-25 17:27:26 +0800

    fix

--- src/views/NewsSandbox/NewsSandbox.js
+++ src/views/NewsSandbox/NewsSandbox.js
@@ -11,6 +11,7 @@ import {Layout} from "antd";
 import RightManageList from "./right-manage/RightManageList";
 import RightRoleList from "./right-manage/RightRoleList";
 import UserManageList from "./user-manage/UserManageList";
+import UdidManage from "./udid-manage/UDIDManage";
 const {Content, Footer} = Layout;
 
 function NewsSandbox() {
@@ -34,6 +35,7 @@ function NewsSandbox() {
                         {/*<Route path="/right-manage" component={RightManage} exact></Route>*/}
                         <Route path="/right-manage/right/list" component={RightManageList} exact></Route>
                         <Route path="/right-manage/role/list" component={RightRoleList} exact></Route>
+                        <Route path="/news-manage/add" component={UdidManage} exact></Route>
                         <Redirect from="/" to="/home" exact></Redirect>
                         <Route path="*" component={NoPermission}></Route>
                     </Switch>
---
+++ src/views/NewsSandbox/udid-manage/UDIDManage.js
@@ -0,0 +1,67 @@
+import { PageHeader, Button, message, Steps } from "antd";
+import React, { useState } from 'react';
+const { Step } = Steps;
+const steps = [
+    {
+        title: 'First',
+        content: 'First-content',
+    },
+    {
+        title: 'Second',
+        content: 'Second-content',
+    },
+    {
+        title: 'Last',
+        content: 'Last-content',
+    },
+];
+
+function UdidManage(props) {
+    const [current, setCurrent] = useState(0);
+
+    const next = () => {
+        setCurrent(current + 1);
+    };
+
+    const prev = () => {
+        setCurrent(current - 1);
+    };
+    return (
+        <div>
+            <PageHeader
+                title="UDID Manage"
+                subTitle="分步表单"
+            />
+            <Steps current={current}>
+                {steps.map((item) => (
+                    <Step key={item.title} title={item.title} />
+                ))}
+            </Steps>
+            <div className="steps-content">{steps[current].content}</div>
+            <div className="steps-action">
+                {current < steps.length - 1 && (
+                    <Button type="primary" onClick={() => next()}>
+                        Next
+                    </Button>
+                )}
+                {current === steps.length - 1 && (
+                    <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                        Done
+                    </Button>
+                )}
+                {current > 0 && (
+                    <Button
+                        style={{
+                            margin: '0 8px',
+                        }}
+                        onClick={() => prev()}
+                    >
+                        Previous
+                    </Button>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default UdidManage;
